fabe79ac3329c26b3263b4a4434ca668254b9f3d
Camille Dbschr <c.debusscher@gmail.com>
2023-01-13 08:45:23 +0100

    add: exercice 1

---
+++ exercice1/src/Controller/TestController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    #[Route('/test/{age}/{nom}/{prenom}', name: 'app_test', requirements: ['age' => '\d+', 'nom' => '[a-z]{2,30}', 'prenom' => '[a-z]{2,30}'])]
+    public function index(Request $request, int $age, string $prenom, string $nom): Response
+    {
+        return $this->render('test/index.html.twig', [
+            'controller_name' => 'TestController',
+            'nom' => $nom,
+            'prenom' => $prenom,
+            'age' => $age,
+        ]);
+    }
+}
